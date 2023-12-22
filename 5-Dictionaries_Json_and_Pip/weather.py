c36a73deda4fca4932e8385093d50cbb85abc4d6
Hadnan Basilio <basiliohadnan@gmail.com>
2023-12-21 21:24:22 -0300

    Weather api info read and printed

--- 5-Dictionaries_Json_and_Pip/weather.py
+++ 5-Dictionaries_Json_and_Pip/weather.py
@@ -1,8 +1,13 @@
 import requests
 
-url = 'http://api.weatherapi.com/v1/current.json?key=b9f847486cfd4ff3a3c00845232212&q=São Paulo&aqi=no'
+city = 'London'
+
+url = 'http://api.weatherapi.com/v1/current.json?key=b9f847486cfd4ff3a3c00845232212&q='+city+'&aqi=no'
 
 response = requests.get(url)
 weather_json = response.json()
 
-print(weather_json)
+temp = weather_json.get('current').get('temp_c')
+description = weather_json.get('current').get('condition').get('text')
+
+print("Today's weather in", city, "is", description, "and the temperature is", temp, "degrees.")
